3e1fcd811bbc40ea77594c92cc39bb8174adec8e
benja <benjariffo26@gmail.com>
2021-10-29 19:39:56 -0300

    Version 3: Prueba de ramas o versiones registro

---
+++ app/src/main/java/com/example/loginsimple/BranchTest.java
@@ -0,0 +1,14 @@
+package com.example.loginsimple;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BranchTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_branch_test);
+    }
+}
